6bedfcea09c2974ba9615eb690ef2901fbef58e2
Oleksandr Tkachenko <tkachenko.oleksandr@flawless.group>
2024-07-06 19:55:16 +0300

    add fucking carts

---
+++ src/modules/configs/BrandCards.ts
@@ -0,0 +1,358 @@
+import bigcommerce from "../../assets/png/bigcommerce.png";
+import buku from "../../assets/png/buku.jpg";
+import cin7 from "../../assets/png/cin7.png";
+import cratejoy from "../../assets/png/cratejoy.png";
+import creativeLogisticsSolutions from "../../assets/png/creative-logistics-solutions.jpg";
+import cscart from "../../assets/png/cs.cart_.png";
+import curotec from "../../assets/png/curotec.jpg";
+import easyship from "../../assets/png/easyship.jpg";
+import ebay from "../../assets/png/ebay.png";
+import ecwid from "../../assets/png/ecwid.png";
+import ejoov from "../../assets/png/ejoov.jpg";
+import etsy from "../../assets/png/etsy-1.png";
+import freePlatform from "../../assets/png/free-platform.png";
+import fulfillmentCom from "../../assets/png/fulfillment.com_.jpg";
+import heroCart from "../../assets/png/hero-cart-lightbluebg.jpg";
+import magento from "../../assets/png/magento.png";
+import magento2 from "../../assets/png/magento2.png";
+import mirakl from "../../assets/png/mirakl.png";
+import narvar from "../../assets/png/narvar.jpg";
+import netsuite from "../../assets/png/netsuite.png";
+import odbc from "../../assets/png/odbc.png";
+import opencart from "../../assets/png/opencart.png";
+import oracle from "../../assets/png/oracle.png";
+import ordoro from "../../assets/png/ordoro.jpg";
+import paypal from "../../assets/png/paypal.png";
+import prestashop from "../../assets/png/prestashop.png";
+import quickbooks from "../../assets/png/quickbooks.png";
+import rocksolid from "../../assets/png/rocksolid.jpg";
+import shippo from "../../assets/png/shippo.jpg";
+import shipstation from "../../assets/png/shipstation.png";
+import shopify from "../../assets/png/shopify.png";
+import shoppingCartElite from "../../assets/png/shopping-cart-elite.png";
+import squarespace from "../../assets/png/squarespace.png";
+import steals from "../../assets/png/steals.png";
+import tradegeko from "../../assets/png/tradegeko.png";
+import vanlo from "../../assets/png/vanlo.jpg";
+import volusion from "../../assets/png/volusion.png";
+import walmart from "../../assets/png/walmart.png";
+import webhooks from "../../assets/png/webhooks.png";
+import wix from "../../assets/png/wix.png";
+import woocommerce from "../../assets/png/woocommerce-1.png";
+import xcart from "../../assets/png/xcart.png";
+import zencart from "../../assets/png/zen-cart.png";
+import aftership from "../../assets/png/aftership.jpg";
+import cart from "../../assets/png/3dcart.png";
+
+import { Carts } from "./AboutPlatformConfig";
+
+export const carts: Carts[] = [
+  {
+    id: 1,
+    photo: shopify,
+    title: "Shopify",
+    description:
+      "Shopify is the leading cloud-based, multichannel commerce platform designed for small and medium-sized businesses.",
+  },
+  {
+    id: 2,
+    photo: netsuite,
+    title: "NetSuite",
+    description:
+      "NetSuite integrates ERP, CRM and a Database-Driven e-commerce platform in one comprehensive business management suite.",
+  },
+  {
+    id: 3,
+    photo: bigcommerce,
+    title: "BigCommerce",
+    description:
+      "eCommerce platform with tools you need to increase sales and improve your bottom line.",
+  },
+  {
+    id: 4,
+    photo: buku,
+    title: "Buku",
+    description:
+      "Buku Ship is a tech gateway where you can find all the shipping solutions you need for your current workflow.",
+  },
+  {
+    id: 5,
+    photo: cin7,
+    title: "Cin7",
+    description:
+      "Cin7’s eCommerce inventory management integrations include Shopify, WooCommerce and Magento, syncing your website with your warehouses.",
+  },
+  {
+    id: 6,
+    photo: cratejoy,
+    title: "Cratejoy",
+    description:
+      "Cratejoy helps subscription businesses build beautiful websites with a collection of website themes and templates.",
+  },
+  {
+    id: 7,
+    photo: creativeLogisticsSolutions,
+    title: "Creative Logistics Solutions",
+    description:
+      "Creative Logistics Solutions provide fast and flexible shipping software that enables high-growth D2C eCommerce brands for a seamless peak season shipping operation.",
+  },
+  {
+    id: 8,
+    photo: cscart,
+    title: "CS-Cart",
+    description:
+      "CS-Cart is a powerful open-source e-commerce system designed to suit specific requirements of online vendors.",
+  },
+  {
+    id: 9,
+    photo: curotec,
+    title: "Curotec",
+    description:
+      "Curotec takes a high-level approach to technology development that includes business strategy and organizational enrichment.",
+  },
+  {
+    id: 10,
+    photo: easyship,
+    title: "Easyship",
+    description:
+      "Easyship is a shipping platform that accelerates eCommerce. High-growth D2C eCommerce brands use their software to deliver goods.",
+  },
+  {
+    id: 11,
+    photo: ebay,
+    title: "eBay",
+    description:
+      "eBay is a global commerce leader including the Marketplace and Classifieds platforms.",
+  },
+  {
+    id: 12,
+    photo: ecwid,
+    title: "Ecwid",
+    description:
+      "Ecwid E-commerce Shopping Cart is Fast and Easy to Use! Set up an Online Store for WordPress, Drupal, Facebook and Much More!",
+  },
+  {
+    id: 13,
+    photo: ejoov,
+    title: "eJOOV",
+    description:
+      "eJOOV is a third-party logistics provider that helps you scale from hundreds to tens of thousands of orders per day and simplifies their offering to make life easier.",
+  },
+  {
+    id: 14,
+    photo: etsy,
+    title: "Etsy",
+    description:
+      "Etsy is DIY Headquarters. Personalized Gifts. Handmade & Vintage Items. One-of-a-Kind Community.",
+  },
+  {
+    id: 15,
+    photo: freePlatform,
+    title: "Free Platform",
+    description:
+      "A free platform providing tools and features for your eCommerce needs.",
+  },
+  {
+    id: 16,
+    photo: fulfillmentCom,
+    title: "Fulfillment.com",
+    description:
+      "Fulfillment.com (FDC) helps you expand your business, connect to best-in-class technology, improve customer experience and save you money.",
+  },
+  {
+    id: 17,
+    photo: cart,
+    title: "3dcart",
+    description:
+      "3dcart provides eCommerce platform is built to address the business challenges inherent in successfully reaching visitors, engaging shoppers.",
+  },
+  {
+    id: 18,
+    photo: magento2,
+    title: "Magento 2",
+    description:
+      "Magento empowers thousands of retailers and brands with the best eCommerce platforms and flexible cloud solutions to rapidly innovate and grow.",
+  },
+  {
+    id: 19,
+    photo: magento,
+    title: "Magento",
+    description:
+      "Magento empowers thousands of retailers and brands with the best eCommerce platforms and flexible cloud solutions to rapidly innovate and grow.",
+  },
+  {
+    id: 20,
+    photo: mirakl,
+    title: "Mirakl",
+    description:
+      "Mirakl is the leading provider of Marketplace Solutions. The Mirakl Marketplace Platform helps companies scale e-Commerce.",
+  },
+  {
+    id: 21,
+    photo: narvar,
+    title: "Narvar",
+    description:
+      "Narvar goes beyond the ‘buy’ button and helps high-growth D2C eCommerce brands create loyal customers with an integrated tracking experience that customers love and come back for.",
+  },
+  {
+    id: 22,
+    photo: odbc,
+    title: "ODBC",
+    description:
+      "ODBC provides database connectivity solutions for your eCommerce platform.",
+  },
+  {
+    id: 23,
+    photo: opencart,
+    title: "OpenCart",
+    description:
+      "A free shopping cart system. OpenCart is an open-source PHP-based online e-commerce solution.",
+  },
+  {
+    id: 24,
+    photo: oracle,
+    title: "Oracle",
+    description:
+      "Oracle’s cloud-based warehouse management software for the fulfillment of eCommerce brands.",
+  },
+  {
+    id: 25,
+    photo: ordoro,
+    title: "Ordoro",
+    description:
+      "Ordoro helps high-volume D2C eCommerce brands optimize their customer experience with shipping and fulfillment technologies that are ready to scale.",
+  },
+  {
+    id: 26,
+    photo: paypal,
+    title: "PayPal",
+    description:
+      "PayPal’s most advanced technologies, tools, services, and financing to participate in global commerce. Designed for marketplaces, eCommerce solution providers.",
+  },
+  {
+    id: 27,
+    photo: prestashop,
+    title: "PrestaShop",
+    description:
+      "PrestaShop is an efficient and innovative e-commerce solution with all the features you need to create an online store and grow your business.",
+  },
+  {
+    id: 28,
+    photo: quickbooks,
+    title: "QuickBooks",
+    description:
+      "QuickBooks integrates with the most popular e-commerce platforms. Securely accept payments through 128-bit SSL encryption.",
+  },
+  {
+    id: 29,
+    photo: rocksolid,
+    title: "Rock Solid Internet Shipping",
+    description:
+      "Rock Solid Internet Shipping is a customized parcel shipping platform that handles all of your shipping management needs, making your business operations run more smoothly.",
+  },
+  {
+    id: 30,
+    photo: shippo,
+    title: "Shippo",
+    description:
+      "Shippo helps e-commerce businesses, online marketplaces, and platforms integrate shipping with multiple carriers through their API and web application.",
+  },
+  {
+    id: 31,
+    photo: shipstation,
+    title: "ShipStation",
+    description:
+      "Shipping software for eCommerce fulfillment with intervenors management.",
+  },
+  {
+    id: 32,
+    photo: shoppingCartElite,
+    title: "Shopping Cart Elite",
+    description:
+      "Shopping Cart Elite is a full-fledged e-commerce powerhouse that caters to small and mid-sized businesses.",
+  },
+  {
+    id: 33,
+    photo: squarespace,
+    title: "Squarespace",
+    description:
+      "Squarespace offers everything you need to start selling online and grow your business. Types: Domains, Websites, Online Stores.",
+  },
+  {
+    id: 34,
+    photo: steals,
+    title: "Steals.com",
+    description:
+      "Steals.com is a unique blend of eCommerce and online media that delivers top-quality brands and products one day at a time to their online communities.",
+  },
+  {
+    id: 35,
+    photo: tradegeko,
+    title: "TradeGecko",
+    description:
+      "Tradegecko provides eCommerce Inventory Management with TradeGecko Integrations with the most powerful eCommerce Platforms in the market.",
+  },
+  {
+    id: 36,
+    photo: vanlo,
+    title: "Vanlo",
+    description:
+      "Vanlo’s integration with cutting-edge shipping carriers and shipping technology empower their high-growth D2C eCommerce clients to launch, grow, and scale more quickly.",
+  },
+  {
+    id: 37,
+    photo: volusion,
+    title: "Volusion",
+    description:
+      "With Volusion you can build and manage your online store with shopping cart software that’s full of easy-to-use tools – no coding required.",
+  },
+  {
+    id: 38,
+    photo: walmart,
+    title: "Walmart",
+    description:
+      "Walmart created a seamless experience to let customers shop anytime and anywhere online.",
+  },
+  {
+    id: 39,
+    photo: webhooks,
+    title: "Webhooks",
+    description:
+      "A webhook in web development is a method of augmenting or altering the behavior of a web page or web application with custom callbacks.",
+  },
+  {
+    id: 40,
+    photo: wix,
+    title: "Wix",
+    description:
+      "Wix Stores gives you all the tools you need to create a beautiful online store.",
+  },
+  {
+    id: 41,
+    photo: woocommerce,
+    title: "WooCommerce",
+    description:
+      "WooCommerce is a free WordPress plugin that adds e-commerce functionality to your WordPress website so you can have an online store.",
+  },
+  {
+    id: 42,
+    photo: xcart,
+    title: "X-Cart",
+    description:
+      "X-Cart is an eCommerce platform created for your business practices. Big or small, x-cart customizable features grow as your digital footprint expands.",
+  },
+  {
+    id: 43,
+    photo: aftership,
+    title: "Aftership",
+    description:
+      "Aftership is a tracking solution for eCommerce businesses and provides a best-in-class post-purchase experience to drive customer loyalty and additional sales.",
+  },
+  {
+    id: 44,
+    photo: zencart,
+    title: "Zen Cart",
+    description:
+      "Zen Cart is an online store management system with an open-source shopping cart.",
+  },
+];
---
+++ src/pages/Integrations.tsx
@@ -0,0 +1,11 @@
+import { BrandCarts } from "modules/BrandCarts";
+
+export const Integrations = () => {
+  return (
+    <article className="flex flex-col items-center">
+      <BrandCarts />
+    </article>
+  );
+};
+
+export default Integrations;
